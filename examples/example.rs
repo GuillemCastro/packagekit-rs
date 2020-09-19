0de440f39a4486031f88df64084c93f4ebd14207
Guillem Castro <guillemcastro4@gmail.com>
2020-09-19 18:34:27 +0200

    Moved example

--- examples/example.rs
+++ examples/example.rs
@@ -1,7 +1,6 @@
 use packagekit::PackageKit;
-//https://www.freedesktop.org/software/PackageKit/pk-using.html
+
 fn main() {
-    println!("arch: {}", std::env::consts::ARCH);
     let pk = PackageKit::new();
     let results = pk.search_package("w3m").unwrap();
     println!("Search results: {:?}", &results);
